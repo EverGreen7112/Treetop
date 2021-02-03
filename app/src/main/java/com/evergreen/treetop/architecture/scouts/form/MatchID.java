bcb301126526164581dc23c9c3e7da55aa3b7fea
Atai Ambus <atayambus@gmail.com>
2021-02-03 11:47:34 +0200

    [◌] feat(arch/scout): add MatchID class
    FUNCTION:
    Can categorize and sort objects by match order including stage and number, use MatchID class
    
    METHOD:
    - Create a MatchID class
    - Create an inner enum MatchType to enumerate competition stages (qualifications and playoff)
    - Add number and type members to MatchID
    - Create after and before classes methods and implement equals object method  to compare between two mathces
    
    COMPILES: Yes
    RUNS: Yes
    WORKS: unchecked
    BUGS: unchecked
    
    NOTES:
    Could not get sysout and log to show, so no testing yet.

---
+++ app/src/main/java/com/evergreen/treetop/architecture/scouts/form/MatchID.java
@@ -0,0 +1,53 @@
+package com.evergreen.treetop.architecture.scouts.form;
+
+import java.util.Objects;
+
+public class MatchID {
+
+    enum MatchType {
+        QUAL,
+        PLAYOFF;
+    }
+
+    private MatchType m_type;
+    private int m_number;
+
+    public MatchID(MatchType type, int number) {
+        m_type = type;
+        m_number = number;
+    }
+
+    public MatchType getType() {
+        return  m_type;
+    }
+
+    public int getNumber() {
+        return m_number;
+    }
+
+    public boolean after(MatchID match) {
+        if (m_type.compareTo(match.getType()) == 0) {
+            return m_number > match.getNumber();
+        }
+
+        return  m_type.compareTo(match.getType()) > 0;
+    }
+
+    public boolean before(MatchID match) {
+        if (m_type.compareTo(match.getType()) == 0) {
+            return m_number < match.getNumber();
+        }
+
+        return  m_type.compareTo(match.getType()) < 0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MatchID)) return false;
+        MatchID matchID = (MatchID) o;
+        return m_number == matchID.m_number &&
+                m_type == matchID.m_type;
+    }
+
+}
