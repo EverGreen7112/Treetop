e38d78c4684f063b14e420629cbe92670f172a61
Tom Dotan <lowkitrump@gmail.com>
2021-02-05 17:15:19 +0200

    [✓] ref(ui/frags/form): rename file to match function
    PREVIOUSLY:
    Named "SC_FormAutoTab"
    
    NOW:
    Named "SC_FormCountersFragment"
    
    BECAUSE:
    The file is used to populate the form counters fragment in different layouts.
    
    NOTES:
    Might need to add "SC_FormAutoTab" back, depending on implementation.
    
    —
    
    COMPILES: Yes
    RUNS: Yes
    WORKS: Yes
    BUGS: None

---
+++ app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormCountersFragment.java
@@ -0,0 +1,20 @@
+package com.evergreen.treetop.ui.fragments.form;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.evergreen.treetop.R;
+
+public class SC_FormCountersFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_powercell_counters_sc, container, false);
+    }
+}
