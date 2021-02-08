37d9abbf2a72a4e1005dc386184c65e32cf7db3e
Atai Ambus <atayambus@gmail.com>
2021-02-08 13:19:41 +0200

    `[✓] feat(): add testing activity`
    **FUNCTION:**
    - Setting value TEST in FormLauncher to true will make the app lunch immediately into a new activity  called TestActivity
    
    **METHOD:**
    - Added boolean variable TEST in FormLauncher
    - Added an activity called TestActivity
    - Added a conditional in FormLauncher to change into TestActivity immediately in OnCreate call if TEST is set to true
    
    **NOTES:**
    - Use this class for all your architecture testing, so we don't needlessly mess around with classes we actually use 
    
    —
    
    **COMPILES:** Yes
    **RUNS:** Yes
    **WORKS:** Yes
    **BUGS:** None

--- app/src/main/java/com/evergreen/treetop/activities/scouts/form/SC_FormLauncher.java
+++ app/src/main/java/com/evergreen/treetop/activities/scouts/form/SC_FormLauncher.java
@@ -2,37 +2,29 @@ package com.evergreen.treetop.activities.scouts.form;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.evergreen.treetop.R;
-import com.evergreen.treetop.architecture.scouts.form.MatchID;
+import com.evergreen.treetop.architecture.scouts.utils.MatchID;
+import com.evergreen.treetop.test.TestActivity;
+import com.google.firebase.FirebaseApp;
 
 public class SC_FormLauncher extends AppCompatActivity {
+
+    private static final boolean TEST = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_launcher_sc);
 
-        MatchID a = new MatchID(MatchID.MatchType.QUAL, 20);
-        MatchID b = new MatchID(MatchID.MatchType.QUAL, 10);
-        MatchID c = new MatchID(MatchID.MatchType.QUAL, 20);
-        MatchID d = new MatchID(MatchID.MatchType.PLAYOFF, 10);
+        if (TEST) {
+            startActivity(new Intent(this, TestActivity.class));
+            return;
+        }
 
-        Log.d("Testing", "===TESTING===");
-
-        Log.d("Testing", a.toString() + " after " + b.toString() + ":" + a.after(b));
-        Log.d("Testing", a.toString() + " before " + b.toString() + ":" + a.before(b));
-        Log.d("Testing", a.toString() + " is " + b.toString() + ":" + a.equals(b));
-
-        Log.d("Testing", a.toString() + " after " + c.toString() + ":" + a.after(c));
-        Log.d("Testing", a.toString() + " before " + c.toString() + ":" + a.before(c));
-        Log.d("Testing", a.toString() + " is " + c.toString() + ":" + a.equals(c));
-
-        Log.d("Testing", a.toString() + " after " + d.toString() + ":" + a.after(d));
-        Log.d("Testing", a.toString() + " before " + d.toString() + ":" + a.before(d));
-        Log.d("Testing", a.toString() + " is " + d.toString() + ":" + a.equals(d));
     }
 
 
---
+++ app/src/main/java/com/evergreen/treetop/test/TestActivity.java
@@ -0,0 +1,26 @@
+package com.evergreen.treetop.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.evergreen.treetop.R;
+import com.evergreen.treetop.architecture.scouts.form.DropDown;
+import com.evergreen.treetop.architecture.scouts.handlers.MatchDB;
+import com.evergreen.treetop.architecture.scouts.utils.ScoutingMatch;
+import com.evergreen.treetop.ui.custom.spinner.BaseSpinner;
+
+import java.sql.Struct;
+import java.util.HashMap;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+
+    }
+}
