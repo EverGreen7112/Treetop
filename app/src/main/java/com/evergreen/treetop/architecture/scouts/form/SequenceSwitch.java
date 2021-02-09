24d8c455610e22b81b114d006306aa71126cc983
Atai Ambus <atayambus@gmail.com>
2021-02-09 12:14:01 +0200

    `[✓] feat(arch/scout/form): class  wraps submits switch state sequences`
    **FUNCTION:**
    - Initializing a SequenceSwitch class around a SwitchMaterial view and giving it a path and a label will set the label accordingly, and submit the seq
    
    **METHOD:**
    - Created a class SequenceSwitch with SwitchMaterial member, that extends FormObject (and thus is registered under MatchDB)
    - Set label and path apropriately in constructor
    - Keep an ArrayList<TimedAction> member
    - Set the switch onClickListener to keep start times upon setting true and add a matching TimedAction to the list and reset upon setting false.
    - In submit(), send the list of TimedActions to the object's DB reference.
    
    —
    
    **COMPILES:** Yes
    **RUNS:** Yes
    **WORKS:** Yes
    **BUGS:** None

---
+++ app/src/main/java/com/evergreen/treetop/architecture/scouts/form/SequenceSwitch.java
@@ -0,0 +1,53 @@
+package com.evergreen.treetop.architecture.scouts.form;
+
+import android.util.Log;
+
+import com.evergreen.treetop.architecture.scouts.data.TimedAction;
+import com.evergreen.treetop.architecture.scouts.utils.ScoutingMatch;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+import java.util.ArrayList;
+
+public class SequenceSwitch extends FormObject  {
+
+    private final SwitchMaterial m_switch;
+    private final ArrayList<TimedAction> m_sequence = new ArrayList<>();
+    /**
+     * Keep the time the switch was turned on. If the switch is turned off, should be -1.
+     */
+    private int m_onStart = -1;
+
+    public SequenceSwitch(String label, String path, SwitchMaterial switchView) {
+        super(label, path);
+        m_switch = switchView;
+        m_switch.setText(label);
+
+        Log.i("FORM OBJECT", "Initialized new SequenceSwitch \"" + getLabel()
+                + "\" for path " + getPath());
+
+        m_switch.setOnClickListener((v) -> {
+            SwitchMaterial s = (SwitchMaterial)v;
+            if (s.isChecked()) {
+                m_onStart = ScoutingMatch.getCurrent().getTimeSinceStart();
+                Log.i("FORM_EVENT", "SequenceSwitch " + getLabel() + " turned ON. " +
+                        "saving on time to " + m_onStart);
+            } else {
+                TimedAction action = new TimedAction(getLabel(), m_onStart, ScoutingMatch.getCurrent().getTimeSinceStart());
+                Log.d("SEQUENCE_SWITCH", "Timed action " + action.toString() + " created");
+                m_sequence.add(action);
+                Log.d("SEQUENCE_SWITCH", "Timed action " + action.toString() + "added to list.");
+                Log.i("FORM_EVENT", "SequenceSwitch \"" + getLabel() + "\" turned OFF. " +
+                        "saving on time to " + m_onStart);
+                m_onStart = -1;
+
+            }
+        });
+    }
+
+    @Override
+    public void submit() {
+        getRef().setValue(m_sequence);
+        Log.i("DB_EVENT", "Submitting SequenceSwitch \"" + getLabel() + "\" to path " +
+                getPath()  +  " under value " + m_sequence.toString());
+    }
+}
