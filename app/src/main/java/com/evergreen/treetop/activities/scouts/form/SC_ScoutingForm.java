1e23a221b5bcbf01c4f23bf53c4548fa837956bb
Atai Ambus <atayambus@gmail.com>
2021-02-09 21:46:25 +0200

    `[◌] feat(act/scout/form/c): start FormObject initialization`
    **FUNCTION:**
    - Counter fragment in auto tab now works as intended (increment and decrement, send data properlyy upon submission)
    - Function switch and Passed the Line switch function as intended (send correct data upon submission)
    
    **METHOD:**
    - Call autonomous counters fragment's init(GameStage) method with GameStage.AUTO to initialize its objects
    - Create FormSwitch and SequenceSwitch objects for the Functions and Passed the line switches.
    
    —
    
    **COMPILES:** Yes
    **RUNS:** Yes
    **WORKS:** **Unchecked**
    **BUGS:** **Unchecked**

--- app/src/main/java/com/evergreen/treetop/activities/scouts/form/SC_ScoutingForm.java
+++ app/src/main/java/com/evergreen/treetop/activities/scouts/form/SC_ScoutingForm.java
@@ -4,9 +4,15 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.evergreen.treetop.R;
+import com.evergreen.treetop.architecture.scouts.data.GameStage;
+import com.evergreen.treetop.architecture.scouts.form.FormCounter;
+import com.evergreen.treetop.architecture.scouts.form.FormSwitch;
+import com.evergreen.treetop.architecture.scouts.form.SequenceSwitch;
+import com.evergreen.treetop.ui.fragments.form.SC_FormCountersFragment;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
@@ -18,6 +24,15 @@ public class SC_ScoutingForm extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tab_auto_sc);
+
+        SC_FormCountersFragment counterFrag =
+                (SC_FormCountersFragment)getSupportFragmentManager().findFragmentById(R.id.sc_form_frag_auto_counter);
+
+        counterFrag.init(GameStage.AUTO);
+
+        new FormSwitch("Passed the Line", "auto/line-pass", findViewById(R.id.sc_form_line_passed_switch), findViewById(R.id.sc_form_line_passed_label));
+        new SequenceSwitch("Functions", "auto/function", findViewById(R.id.sc_form_function_switch), findViewById(R.id.sc_form_function_label));
+//        new FormCounter("Initial Cell Load", "auto/init-load", findViewById(R.id.sc_form_loaded_counter_valuebox), findViewById(R.id.loaded));
 
 //        tabLayout = findViewById(R.id.sc_scouting_form_tab_layout);
 //        viewPager = findViewById(R.id.sc_scouting_form_view_pager);
