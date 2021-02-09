6df8764a240b603a23c5107995f7862515bf2f13
Atai Ambus <atayambus@gmail.com>
2021-02-09 12:32:55 +0200

    feat(arch/scout/form): add numberbox class
    This takes two text views, sets a label, makes sure the input type is number and sends it to the database as int upon submission.

---
+++ app/src/main/java/com/evergreen/treetop/architecture/scouts/form/NumberBox.java
@@ -0,0 +1,27 @@
+package com.evergreen.treetop.architecture.scouts.form;
+
+import android.text.InputType;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class NumberBox extends FormObject {
+    private final EditText m_inputBox;
+
+    public NumberBox(String label, String path, EditText inputBox, TextView labelView) {
+        super(label, path);
+        labelView.setText(label);
+
+        m_inputBox = inputBox;
+        m_inputBox.setInputType(InputType.TYPE_CLASS_NUMBER);
+        Log.i("FORM_OBJECT", "Initialized new NumberBox \"" + getLabel() + "\" at path " + getPath());
+    }
+
+    @Override
+    public void submit() {
+        int val = Integer.parseInt(m_inputBox.getText().toString());
+        getRef().setValue(val);
+        Log.i("DB_EVENT", "Submitted NumberBox \"" + getLabel() + "\" with value "
+                + val + " under path " + getPath());
+    }
+}
