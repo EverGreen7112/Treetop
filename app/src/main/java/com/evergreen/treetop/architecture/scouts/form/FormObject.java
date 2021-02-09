486858044590b06c2883748310668d580832f2ce
Atai Ambus <atayambus@gmail.com>
2021-02-09 08:56:29 +0200

    [✓] feat(arch/scout/form): create basic FormObject class
    **FUNCTION:**
    - Can now use FormObject as a baseline for any object on the form (counters, switches, etc.); To group their label and path, and submit them
    
    **METHOD:**
    - Edited FormObject class to include path and label members
    - Initialized them in constructor
    - Added proper toString() method
    
    **NOTES:**
    This is an abstract class and needs to be implemented for each individual FormObject type.
    
    —
    
    **COMPILES:** Yes
    **RUNS:** Yes
    **WORKS:** Yes
    **BUGS:** None

--- app/src/main/java/com/evergreen/treetop/architecture/scouts/form/FormObject.java
+++ app/src/main/java/com/evergreen/treetop/architecture/scouts/form/FormObject.java
@@ -1,7 +1,30 @@
 package com.evergreen.treetop.architecture.scouts.form;
 
-public abstract class FormObject {
-    public FormObject(){
+import com.evergreen.treetop.architecture.scouts.handlers.MatchDB;
+import com.evergreen.treetop.architecture.scouts.utils.Loggable;
+import com.evergreen.treetop.architecture.scouts.utils.ScoutingMatch;
+import com.google.firebase.database.DatabaseReference;
+
+public abstract class FormObject implements Loggable {
+
+    private final String m_path;
+    private final String m_label;
+
+    public FormObject(String label, String path) {
+        m_path = path;
+        m_label = label;
+        MatchDB.activateObject(this);
+    }
+
+    public abstract void submit();
+
+    @Override
+    public String toString() {
+        return m_label + " at " + m_path;
+    }
+
+    public String getLabel() {
+        return m_label;
 
     }
 }
