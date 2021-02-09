d7254fafaf1200061d27e34863f45ca6a3006ee1
Atai Ambus <atayambus@gmail.com>
2021-02-09 21:34:15 +0200

    `[◌] feat(ui/frag/form): add initialization method to counters fragment`
    **FUNCTION:**
    Calling init(GameStage) on an SC_FormCountersFragment object will fully initialize the included counters as HitMissCounter objects, under a folder for the input GameStage
    
    **METHOD:**
    - init(GameStage) simply creates the matching object for each increment-decrements increment-decrements label group of TextVIews.
    
    —
    
    **COMPILES:** Yes
    **RUNS:** Yes
    **WORKS:** **Unchecked**
    **BUGS:** **Unchecked**

--- app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormCountersFragment.java
+++ app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormCountersFragment.java
@@ -10,11 +10,71 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.evergreen.treetop.R;
+import com.evergreen.treetop.architecture.scouts.data.GameStage;
+import com.evergreen.treetop.architecture.scouts.form.HitMissCounter;
 
 public class SC_FormCountersFragment extends Fragment {
+
+    private boolean m_initialized;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_powercell_counters_sc, container, false);
+    }
+
+    public void init(GameStage stage) {
+        String pathPrefix = stage.getName();
+
+        new HitMissCounter(
+                "Bottom",
+                pathPrefix + "/bottom",
+                getView().findViewById(R.id.sc_text_form_counters_bot_label),
+                getView().findViewById(R.id.sc_text_form_counters_bot_hit_value),
+                getView().findViewById(R.id.sc_text_form_counters_bot_hit_decrement),
+                getView().findViewById(R.id.sc_text_form_counters_bot_miss_value),
+                getView().findViewById(R.id.sc_text_form_counters_bot_miss_decrement)
+        );
+
+        new HitMissCounter(
+                "Outer",
+                pathPrefix + "/outer",
+                getView().findViewById(R.id.sc_text_form_counters_out_label),
+                getView().findViewById(R.id.sc_text_form_counters_out_hit_value),
+                getView().findViewById(R.id.sc_text_form_counters_out_hit_decrement),
+                getView().findViewById(R.id.sc_text_form_counters_out_miss_value),
+                getView().findViewById(R.id.sc_text_form_counters_out_miss_decrement)
+        );
+
+        new HitMissCounter(
+                "Inner",
+                pathPrefix + "/inner",
+                getView().findViewById(R.id.sc_text_form_counters_in_label),
+                getView().findViewById(R.id.sc_text_form_counters_in_hit_value),
+                getView().findViewById(R.id.sc_text_form_counters_in_hit_decrement),
+                getView().findViewById(R.id.sc_text_form_counters_in_miss_value),
+                getView().findViewById(R.id.sc_text_form_counters_in_miss_decrement)
+        );
+
+        new HitMissCounter(
+                "Load",
+                pathPrefix + "/load",
+                getView().findViewById(R.id.sc_text_form_counters_load_label),
+                getView().findViewById(R.id.sc_text_form_counters_load_hit_value),
+                getView().findViewById(R.id.sc_text_form_counters_load_hit_decrement),
+                getView().findViewById(R.id.sc_text_form_counters_load_miss_value),
+                getView().findViewById(R.id.sc_text_form_counters_load_miss_decrement)
+        );
+
+        new HitMissCounter(
+                "Collect",
+                pathPrefix + "/collection",
+                getView().findViewById(R.id.sc_text_form_counters_coll_label),
+                getView().findViewById(R.id.sc_text_form_counters_coll_hit_value),
+                getView().findViewById(R.id.sc_text_form_counters_coll_hit_decrement),
+                getView().findViewById(R.id.sc_text_form_counters_coll_miss_value),
+                getView().findViewById(R.id.sc_text_form_counters_coll_miss_decrement)
+        );
+
     }
 }
