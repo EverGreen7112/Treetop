5f9634aad9a986d5aea89aa3baab0f1f019eb618
Atai Ambus <atayambus@gmail.com>
2021-02-10 21:24:12 +0200

    `[✓] feat(ui/scout/frag/form): add object initialization for top bar`
    **FUNCTION:**
    Inserting the form header fragment will automatically start the match timer and initialize the function switch as a SequenceSwitch FormObject
    
    **METHOD:**
    - Run in fragment onCreateView, keeping the built view instead of returning it directly.
    -  Update the match timer every second with the milli count already in ScoutingMatch.getCurrent() and a runnable that posts its next interval to the handle in each run until the match ends.
    - Initialize the function switch under the current GameStage using GameStage.getCurrentStage()
     
    ―
    
    **COMPILES:** Yes
    **RUNS:** Yes
    **WORKS:** Yes
    **BUGS:** None

--- app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormHeaderFragment.java
+++ app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormHeaderFragment.java
@@ -12,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.evergreen.treetop.R;
+import com.evergreen.treetop.architecture.scouts.data.GameStage;
+import com.evergreen.treetop.architecture.scouts.form.SequenceSwitch;
 import com.evergreen.treetop.architecture.scouts.utils.ScoutingMatch;
 
 import java.util.Locale;
@@ -21,6 +23,41 @@ public class SC_FormHeaderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_form_header, container, false);
+        View thisView =  inflater.inflate(R.layout.fragment_form_header, container, false);
+
+
+        TextView matchTimer = thisView.findViewById(R.id.sc_form_auto_match_timer);
+        Handler handler = new Handler();
+
+        Runnable counter = new Runnable() {
+            @Override
+            public void run() {
+                long matchSeconds = ScoutingMatch.getCurrent().getTimeSinceStart() / 1000;
+                if (matchSeconds <= 3 * 60) {
+                    matchTimer.setText(formatTimer(matchSeconds));
+                    handler.postDelayed(this, 1000);
+                    Log.v("UI_EVENT", "Set Match Timer (Auto) to  " + formatTimer(matchSeconds));
+                    Log.v("UI_EVENT", "Posting autonomous Match Timer increment");
+                }
+            }
+        };
+
+        handler.postDelayed(counter, 1000);
+        Log.v("UI_EVENT", "Posting autonomous Match Timer increment");
+        return thisView;
+
+
+        new SequenceSwitch(
+                "Functions",
+                GameStage.getCurrentStage().getName().toLowerCase() + "/function",
+                thisView.findViewById(R.id.sc_form_auto_function_switch),
+                thisView.findViewById(R.id.sc_form_auto_function_label)
+        );
+    }
+
+    private String formatTimer(long seconds) {
+        long mins = seconds / 60;
+        long secs = seconds % 60;
+        return String.format(Locale.ENGLISH, "%02d:%02d", mins, secs);
     }
 }
