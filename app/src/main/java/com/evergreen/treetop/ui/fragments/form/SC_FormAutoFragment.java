b2de630630378360562e5a88e7134c61d25f5769
Atai Ambus <atayambus@gmail.com>
2021-02-10 21:39:37 +0200

    `[✓] feat(ui/frag/scout/form): activate autonomous form objects`
    **FUNCTION:**
    - Whenever the autonomous tab fragment is inserted, all of its counters and switches will be automatically activated as their matching FormObject instances.
    
    **METHOD:**
    - Add constant GAME_STAGE = GameStage.AUTO
    - In onCreateView, keep view instead of returning it immediately.
    - Find the counters fragment by id with getChildFragmentManager() and call its init method with GAME_STAGE
    - Find the appropriate switch in the kept view and initialize it as FormSwitch.
    
    ―
    
    **COMPILES:** Yes
    **RUNS:** Yes
    **WORKS:** Yes
    **BUGS:** None

--- app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormAutoFragment.java
+++ app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormAutoFragment.java
@@ -1,6 +1,7 @@
 package com.evergreen.treetop.ui.fragments.form;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,12 +11,41 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.evergreen.treetop.R;
+import com.evergreen.treetop.architecture.scouts.data.GameStage;
+import com.evergreen.treetop.architecture.scouts.form.FormSwitch;
+import com.evergreen.treetop.architecture.scouts.handlers.MatchDB;
 
 public class SC_FormAutoFragment extends Fragment {
 
+    GameStage GAME_STAGE = GameStage.AUTO;
     @Nullable
+
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_tab_auto_sc, container, false);
+        View res =  inflater.inflate(R.layout.activity_tab_auto_sc, container, false);
+        initObjects(res);
+        Log.i("FORM_EVENT", "Initialized Autonomous Tab");
+        res.findViewById(R.id.submit_form).setOnClickListener( v -> MatchDB.submitActiveForm());
+        return res;
     }
+
+    private void initObjects(View thisView) {
+        String pathPrefix = GAME_STAGE.getName().toLowerCase();
+        SC_FormCountersFragment counters =
+                (SC_FormCountersFragment) getChildFragmentManager().findFragmentById(R.id.sc_form_frag_auto_counters);
+        counters.init(GAME_STAGE);
+
+        new FormSwitch(
+                "Passed the Line",
+                pathPrefix + "/line-pass",
+                thisView.findViewById(R.id.sc_form_auto_line_passed_switch),
+                thisView.findViewById(R.id.sc_form_auto_line_passed_label)
+        );
+
+
+
+
+
+    }
+
 }
