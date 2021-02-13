bacdbc57cb200384e8b5f7db1c80545b0e9a9312
Atai Ambus <atayambus@gmail.com>
2021-02-13 11:45:39 +0200

    `[✓] feat(act/scout/form): activate teleop, end tabs`
    **FUNCTION:**
    -  Form objects in Teleop, endgame tabs now enable ui interactions, send data on submission
    
    **METHOD:**
    - Simple initialization of FormObject inheriting instances in onCreateView with matching views and path.
    
    —
    
    **COMPILES:** Yes
    **RUNS:** Yes
    **WORKS:** Partially. Views reset after moving each tab (because the fragment view is fully recreated upon change, therefore onCreateView runs and recreates everything)

--- app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormEndgameFragment.java
+++ app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormEndgameFragment.java
@@ -10,13 +10,68 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.evergreen.treetop.R;
+import com.evergreen.treetop.architecture.scouts.data.GameStage;
+import com.evergreen.treetop.architecture.scouts.form.ContinousStopwatch;
+import com.evergreen.treetop.architecture.scouts.form.FormSwitch;
+import com.evergreen.treetop.architecture.scouts.form.SequenceStopwatch;
 
 public class SC_FormEndgameFragment extends Fragment {
+
+    GameStage GAME_STAGE = GameStage.ENDGAME;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_tab_end_sc, container, false);
+        View thisView =  inflater.inflate(R.layout.activity_tab_end_sc, container, false);
+
+        String pathPrefix = GAME_STAGE.getName().toLowerCase();
+
+        SC_FormCountersFragment counters =
+                (SC_FormCountersFragment) getChildFragmentManager().findFragmentById(R.id.sc_form_frag_end_counters);
+        counters.init(GAME_STAGE);
+
+        new FormSwitch(
+                "Hanging",
+                pathPrefix + "/hang",
+                thisView.findViewById(R.id.sc_form_end_hanging_switch),
+                thisView.findViewById(R.id.sc_form_end_hanging_label)
+        );
+
+        new ContinousStopwatch(
+                "Hang Attempt",
+                pathPrefix + "/hang-attempt",
+                thisView.findViewById(R.id.sc_form_end_hang_attempt_stopwatch),
+                thisView.findViewById(R.id.sc_form_end_hang_attempt_stopwatch)
+        );
+
+        new FormSwitch(
+                "Scales are Level",
+                pathPrefix + "/level",
+                thisView.findViewById(R.id.sc_form_end_level_switch),
+                thisView.findViewById(R.id.sc_form_end_level_label)
+        );
+
+        new FormSwitch(
+                "Multiple Robots Hang",
+                pathPrefix + "/hang-many",
+                thisView.findViewById(R.id.sc_form_end_multi_hang_switch),
+                thisView.findViewById(R.id.sc_form_end_multi_hang_label)
+        );
+
+        new SequenceStopwatch(
+                "Defense",
+                pathPrefix + "/defense",
+                thisView.findViewById(R.id.sc_form_end_defence_stopwatch),
+                thisView.findViewById(R.id.sc_form_end_defence_label)
+        );
+
+        return  thisView;
+
+
     }
 
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
 }
--- app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormTeleopFragment.java
+++ app/src/main/java/com/evergreen/treetop/ui/fragments/form/SC_FormTeleopFragment.java
@@ -10,13 +10,68 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.evergreen.treetop.R;
+import com.evergreen.treetop.architecture.scouts.data.GameStage;
+import com.evergreen.treetop.architecture.scouts.form.FormCounter;
+import com.evergreen.treetop.architecture.scouts.form.FormSwitch;
+import com.evergreen.treetop.architecture.scouts.form.SequenceStopwatch;
 
 public class SC_FormTeleopFragment extends Fragment {
+
+    GameStage GAME_STAGE = GameStage.TELEOP;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_tab_teleop_sc, container, false);
+        View thisView =  inflater.inflate(R.layout.activity_tab_teleop_sc, container, false);
+
+        // === FORM OBJECT INIT ===
+        String pathPrefix = GAME_STAGE.getName().toLowerCase();
+
+        SC_FormCountersFragment counters =
+                (SC_FormCountersFragment) getChildFragmentManager().findFragmentById(R.id.sc_form_frag_teleop_counters);
+        counters.init(GAME_STAGE);
+
+        // --- Initialize Defense Stopwatch ---
+        new SequenceStopwatch(
+                "Defense",
+                pathPrefix + "/defense",
+                thisView.findViewById(R.id.sc_form_teleop_defence_stopwatch),
+                thisView.findViewById(R.id.sc_form_teleop_defence_label)
+        );
+        // --- --- ---
+        // --- Initialize Rotation Control Switch-Miss ---
+        new FormSwitch(
+                "Rotation Control",
+                pathPrefix + "/rotation-control/hit",
+                thisView.findViewById(R.id.sc_form_teleop_rotation_switch),
+                thisView.findViewById(R.id.sc_form_teleop_rotation_label)
+        );
+
+        new FormCounter(
+                "Miss",
+                pathPrefix + "/rotation-control/miss",
+                thisView.findViewById(R.id.sc_form_teleop_rotation_miss_value),
+                thisView.findViewById(R.id.sc_form_teleop_rotation_miss_decrement)
+        );
+        // --- --- ---
+        // --- Initialize Position Control Switch-Miss
+        new FormSwitch(
+                "Position Control",
+                "position-control/hit",
+                thisView.findViewById(R.id.sc_form_teleop_position_switch),
+                thisView.findViewById(R.id.sc_form_teleop_position_label)
+        );
+
+        new FormCounter(
+                "Miss",
+                "position-control/miss",
+                thisView.findViewById(R.id.sc_form_teleop_position_miss_value),
+                thisView.findViewById(R.id.sc_form_teleop_position_miss_decrement)
+        );
+        // --- --- ---
+        // === === ===
+
+        return thisView;
     }
 
 }
